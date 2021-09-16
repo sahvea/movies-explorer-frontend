2f50d28826afa6a6c54804de57eec2e1f0b8acbd
Sophie <sophie.milash@yandex.ru>
2021-09-16 23:20:58 +0200

    feat: add AboutProject section

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -4,7 +4,30 @@ function AboutProject() {
   return (
     <section id="about" className="about page__section">
       <h2 className="page__section-title">О проекте</h2>
-
+      <ul className="about__desc-list">
+        <li className="about__desc-lis-item">
+          <h3 className="about__desc-title">Дипломный проект включал 5 этапов</h3>
+          <p className="about__desc-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </li>
+        <li className="about__desc-lis-item">
+          <h3 className="about__desc-title">На выполнение диплома ушло 5 недель</h3>
+          <p className="about__desc-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </li>
+      </ul>
+      <table className="about__table">
+        <thead>
+          <tr>
+            <th className="about__table-header">1 неделя</th>
+            <th className="about__table-header">4 недели</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="about__table-cell">Back-end</td>
+            <td className="about__table-cell">Front-end</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   )
 }

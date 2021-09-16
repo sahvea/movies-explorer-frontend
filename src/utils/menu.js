615127e3fcdb24f51c1276fd05fe8a0330b0a1fc
Sophie Milash <sophie.milash@yandex.ru>
2021-09-16 12:51:38 +0200

    feat: add Header's state change

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -9,3 +9,22 @@ export function toggleHeaderBurger() {
   navigation.classList.toggle('navigation_visible');
   navigationMenu.classList.toggle('navigation__menu_active');
 }
+
+export function switchClass() {
+  const TABLET_WIDTH = 1024;
+  const headerBurgerLines = document.querySelectorAll('.header__burger-line');
+  const headerBurgerLineActive = document.querySelector('.header__burger-line_active');
+  const navigation = document.querySelector('.navigation');
+  const navigationMenu = document.querySelector('.navigation__menu');
+
+  if (window.innerWidth <= TABLET_WIDTH && navigation) {
+    navigation.classList.remove('navigation_visible');
+    navigationMenu.classList.remove('navigation__menu_active');
+  }
+
+  if (headerBurgerLineActive) {
+    headerBurgerLines.forEach(line => {
+      line.classList.remove('header__burger-line_active');
+    });
+  }
+}
